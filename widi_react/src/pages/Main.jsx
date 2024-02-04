351162951efa2b2c69a4c268e70c6bdb288af000
Yongsang Lee <restart916@gmail.com>
2024-02-05 01:31:32 +0900

    react build test

--- widi_react/src/pages/Main.jsx
+++ widi_react/src/pages/Main.jsx
@@ -6,6 +6,26 @@ import { fabric } from 'fabric';
 import { logEvent } from "firebase/analytics";
 import { analytics } from '../firebase';
 
+import HeaderRound from '../image/header_round.svg';
+import BG_21 from '../image/bg_21.png';
+import BG_22 from '../image/bg_22.png';
+import BG_23 from '../image/bg_23.png';
+import BG_24 from '../image/bg_24.png';
+import BG_25 from '../image/bg_25.png';
+import BG_26 from '../image/bg_26.png';
+import BG_20 from '../image/bg_20.png';
+import BG_18 from '../image/bg_18.png';
+import BG_17 from '../image/bg_17.png';
+import BG_16 from '../image/bg_16.png';
+import BG_10 from '../image/bg_10.png';
+import BG_9 from '../image/bg_9.png';
+import BG_13 from '../image/bg_13.png';
+import BG_15 from '../image/bg_15.png';
+import BG_11 from '../image/bg_11.png';
+import BG_12 from '../image/bg_12.png';
+import BG_4 from '../image/bg_4.png';
+import BG_5 from '../image/bg_5.png';
+import BG_6 from '../image/bg_6.png';
 
 const fontList = [
   {
@@ -27,33 +47,25 @@ const fontList = [
 ]
 
 const imageList = [
-  {"file": "bg_21","fontColor": '#FFFFFF'},
-  {"file": "bg_22","fontColor": '#FFFFFF'},
-  {"file": "bg_23","fontColor": '#000000'},
-  {"file": "bg_24","fontColor": '#000000'},
-  {"file": "bg_25","fontColor": '#000000'},
-  {"file": "bg_26","fontColor": '#FFFFFF'},
-  {"file": "bg_20","fontColor": '#FFFFFF'},
-  {"file": "bg_18","fontColor": '#FFFFFF'},
-  // {"file": "bg_19","fontColor": '#FFFFFF'},
-  {"file": "bg_17","fontColor": '#FFFFFF'},
-  {"file": "bg_16","fontColor": '#000000'},
-  {"file": "bg_10", "fontColor": '#FFFFFF'},
-  {"file": "bg_9", "fontColor": '#000000'},
-  {"file": "bg_13", "fontColor": '#FFFFFF'},
-  // {"file": "bg_14","fontColor": '#FFFFFF'},
-  {"file": "bg_15","fontColor": '#FFFFFF'},
-  {"file": "bg_11", "fontColor": '#FFFFFF'},
-  {"file": "bg_12","fontColor": '#FFFFFF'},
-  // {"file": "bg_0", "fontColor": '#FFFFFF'},
-  // {"file": "bg_1", "fontColor": '#FFFFFF'},
-  // {"file": "bg_2", "fontColor": '#FFFFFF'},
-  // {"file": "bg_3", "fontColor": '#FFFFFF'},
-  {"file": "bg_4", "fontColor": '#FFFFFF'},
-  {"file": "bg_5", "fontColor": '#FFFFFF'},
-  {"file": "bg_6", "fontColor": '#000000'},
-  // {"file": "bg_7", "fontColor": '#FFFFFF'},
-  // {"file": "bg_8", "fontColor": '#FFFFFF'},
+  {"image": BG_21, "file": "bg_21", "fontColor": '#FFFFFF'},
+  {"image": BG_22, "file": "bg_22", "fontColor": '#FFFFFF'},
+  {"image": BG_23, "file": "bg_23", "fontColor": '#000000'},
+  {"image": BG_24, "file": "bg_24", "fontColor": '#000000'},
+  {"image": BG_25, "file": "bg_25", "fontColor": '#000000'},
+  {"image": BG_26, "file": "bg_26", "fontColor": '#FFFFFF'},
+  {"image": BG_20, "file": "bg_20", "fontColor": '#FFFFFF'},
+  {"image": BG_18, "file": "bg_18", "fontColor": '#FFFFFF'},
+  {"image": BG_17, "file": "bg_17", "fontColor": '#FFFFFF'},
+  {"image": BG_16, "file": "bg_16", "fontColor": '#000000'},
+  {"image": BG_10, "file": "bg_10", "fontColor": '#FFFFFF'},
+  {"image": BG_9, "file": "bg_9", "fontColor": '#000000'},
+  {"image": BG_13, "file": "bg_13", "fontColor": '#FFFFFF'},
+  {"image": BG_15, "file": "bg_15", "fontColor": '#FFFFFF'},
+  {"image": BG_11, "file": "bg_11", "fontColor": '#FFFFFF'},
+  {"image": BG_12, "file": "bg_12", "fontColor": '#FFFFFF'},
+  {"image": BG_4, "file": "bg_4", "fontColor": '#FFFFFF'},
+  {"image": BG_5, "file": "bg_5", "fontColor": '#FFFFFF'},
+  {"image": BG_6, "file": "bg_6", "fontColor": '#000000'},
 ]
 
 export default function Main() {
@@ -342,7 +354,7 @@ export default function Main() {
             
         <div className="header">
           <div id='productName'>
-            <img src="./image/header_round.svg" onClick={moveHome} />
+            <img src={HeaderRound} onClick={moveHome} />
           </div>
         </div>
 
@@ -378,12 +390,12 @@ export default function Main() {
             </div>
 
             <div id='selectImage' className="container ps-0 pe-0">
-                {/* imageList for loop */}
                 { imageList.map((image, index) => {
                   return (
                     <div className="item" key={index}>
                       <div onClick={() => onClickImage(image)}>
-                        <img id={image.file} src={'./image/' + image.file + '.png'} className={checkSelected(image)} />
+                        <img id={image.file} src={image.image} alt={image.file}
+                        className={checkSelected(image)} />
                       </div>
                     </div>
                   )})
